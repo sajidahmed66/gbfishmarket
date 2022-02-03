2802257e07d6260e5a5f2d77125184dd2eeb1d7e
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-02-03 23:00:22 +0600

    navbar completed

--- src/components/Common/Layout.tsx
+++ src/components/Common/Layout.tsx
@@ -1,22 +1,20 @@
-import { useEffect } from 'react';
-import NavBar from '../NavBar/NavBar';
+import { useEffect } from "react";
+import NavBar from "../NavBar/NavBar";
 interface ILayoutProps {
-    title: string;
-    children: React.ReactNode;
+  title: string;
+  children: React.ReactNode;
 }
 
 const Layout = ({ children, title }: ILayoutProps) => {
-    useEffect(() => {
-        document.title = title;
-    }, [])
-    return (
-        <div >
-            <NavBar />
-            <div>
-                {children}
-            </div>
-        </div>
-    )
-}
+  useEffect(() => {
+    document.title = title;
+  }, []);
+  return (
+    <div>
+      <NavBar />
+      <div>{children}</div>
+    </div>
+  );
+};
 
 export default Layout;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,177 +1,142 @@
-import { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
-import { GiHamburgerMenu } from 'react-icons/gi';
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiFillCloseCircle } from "react-icons/ai";
+//
 //  raisin black = #2E2C2F
 // deep space Sparkle = #475B63
 // lavender = #FFEAEC
 const NavBar = () => {
-    const [isToggle, setIsToggle] = useState(false);
-    const toggleMenuHandler = () => {
-        setIsToggle(!isToggle);
-        console.log(isToggle);
+  const [isToggle, setIsToggle] = useState(false);
+  const [toggleClass, setToggleClass] = useState(
+    "min-w-max absolute bg-white text-base z-50 top-6 -right-8 py-2 list-none text-left  rounded-lg shadow-lg  mt-1 m-0 bg-clip-padding border-none hidden"
+  );
+  const toggleMenuHandler = () => {
+    if (!isToggle) {
+      setToggleClass(
+        "min-w-max absolute bg-white text-base z-50 top-6 -right-8 py-2 list-none text-left  rounded-lg shadow-lg  mt-1 m-0 bg-clip-padding border-none"
+      );
+    } else {
+      setToggleClass(
+        "min-w-max absolute bg-white text-base z-50 top-6 -right-8 py-2 list-none text-left  rounded-lg shadow-lg  mt-1 m-0 bg-clip-padding border-none hidden"
+      );
     }
+    setIsToggle(!isToggle);
+  };
 
-    return (
-        <nav className='bg-gray-700 h-28 '>
-            <div className='flex flex-row mx-auto items-center justify-between self-center max-w-screen-xl px-12 '>
-                <div className=" h-full w-48 bg-white border border-y-0 border-black  " >
-                    <Link to='/'>
-                        <img src={require('../../assets/img/gbicon.PNG')} alt="LOGO" className='py-3' />
-                    </Link>
-                </div>
-                <ul className='hidden md:flex md:flex-row md:items-center md:space-x-6'>
-                    <li>
-                        <NavLink to='/' className={({ isActive }) => (isActive ? 'text-violet-50' : 'text-black')}>
-                            <span className='text-xl font-bold'>Home</span></NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/aboutus' className={({ isActive }) => (isActive ? 'text-violet-50' : 'text-black')}>
-                            <span className='text-xl font-bold'>About Us</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/products' className={({ isActive }) => (isActive ? 'text-violet-50' : 'text-black')} >
-                            <span className='text-xl font-bold'>Products</span>
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/contactus' className={({ isActive }) => (isActive ? 'text-violet-50' : 'text-black')} >
-                            <span className='text-xl font-bold'>Contact Us</span>
-                        </NavLink>
-                    </li>
-                </ul>
-                <div className="flex justify-center">
-                    <div>
-                        <div className="dropstart relative">
-                            {/* <button
-                                className="
-                                    dropdown-toggle
-                                    px-6
-                                    py-2.5
-                                    bg-blue-600
-                                    text-white
-                                    font-medium
-                                    text-xs
-                                    leading-tight
-                                    uppercase
-                                    rounded
-                                    shadow-md
-                                    hover:bg-blue-700 hover:shadow-lg
-                                    focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-                                    active:bg-blue-800 active:shadow-lg active:text-white
-                                    transition
-                                    duration-150
-                                    ease-in-out
-                                    flex
-                                    items-center
-                                    whitespace-nowrap"
-                                type="button"
-                                id="dropdownMenuButton1s"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false"
-                            >
-                                <svg
-                                    aria-hidden="true"
-                                    focusable="false"
-                                    data-prefix="fas"
-                                    data-icon="caret-left"
-                                    className="w-1.5 mr-2"
-                                    role="img"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 192 512"
-                                >
-                                    <path
-                                        fill="currentColor"
-                                        d="M192 127.338v257.324c0 17.818-21.543 26.741-34.142 14.142L29.196 270.142c-7.81-7.81-7.81-20.474 0-28.284l128.662-128.662c12.599-12.6 34.142-3.676 34.142 14.142z"
-                                    ></path>
-                                </svg>
-                                Dropstart
-                            </button> */}
-                            <GiHamburgerMenu className='h-6 w-6 cursor-pointer hover:fill-indigo-500 md:hidden' style={{ color: '#FFEAEC' }} onClick={toggleMenuHandler} />
-                            <ul
-                                className="
-                                    min-w-max
-                                    absolute
-                                    bg-white
-                                    text-base
-                                    z-50
-                                    float-left
-                                    py-2
-                                    list-none
-                                    text-left
-                                    rounded-lg
-                                    shadow-lg
-                                    mt-1
-                                    m-0
-                                    bg-clip-padding
-                                    border-none
-                                    "
-                                aria-labelledby="dropdownMenuButton1s"
-                            >
-                                <li>
-                                    <a
-                                        className="
-                                                text-sm
-                                                py-2
-                                                px-4
-                                                font-normal
-                                                block
-                                                w-full
-                                                whitespace-nowrap
-                                                bg-transparent
-                                                text-gray-700
-                                                hover:bg-gray-100
-                                                "
-                                        href="#"
-                                    >Action</a
-                                    >
-                                </li>
-                                <li>
-                                    <a
-                                        className="
-                                            dropdown-item
-                                            text-sm
-                                            py-2
-                                            px-4
-                                            font-normal
-                                            block
-                                            w-full
-                                            whitespace-nowrap
-                                            bg-transparent
-                                            text-gray-700
-                                            hover:bg-gray-100
-                                            "
-                                        href="#"
-                                    >Another action</a
-                                    >
-                                </li>
-                                <li>
-                                    <a
-                                        className="
-                                            dropdown-item
-                                            text-sm
-                                            py-2
-                                            px-4
-                                            font-normal
-                                            block
-                                            w-full
-                                            whitespace-nowrap
-                                            bg-transparent
-                                            text-gray-700
-                                            hover:bg-gray-100
-                                            "
-                                        href="#"
-                                    >Something else here</a
-                                    >
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <nav className="bg-gray-700 h-28 ">
+      <div className="flex flex-row mx-auto items-center justify-between self-center max-w-screen-xl px-12 ">
+        <div className=" h-full w-48 bg-white border border-y-0 border-black  ">
+          <Link to="/">
+            <img
+              src={require("../../assets/img/gbicon.PNG")}
+              alt="LOGO"
+              className="py-3"
+            />
+          </Link>
+        </div>
+        <ul className="hidden md:flex md:flex-row md:items-center md:space-x-6">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? "text-violet-50" : "text-black"
+              }
+            >
+              <span className="text-xl font-bold">Home</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/aboutus"
+              className={({ isActive }) =>
+                isActive ? "text-violet-50" : "text-black"
+              }
+            >
+              <span className="text-xl font-bold">About Us</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) =>
+                isActive ? "text-violet-50" : "text-black"
+              }
+            >
+              <span className="text-xl font-bold">Products</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contactus"
+              className={({ isActive }) =>
+                isActive ? "text-violet-50" : "text-black"
+              }
+            >
+              <span className="text-xl font-bold">Contact Us</span>
+            </NavLink>
+          </li>
+        </ul>
+        <div className="flex justify-center">
+          <div>
+            <div className="relative">
+              {isToggle ? (
+                <AiFillCloseCircle
+                  className="h-6 w-6 cursor-pointer hover:fill-indigo-500 md:hidden"
+                  style={{ color: "#FFEAEC" }}
+                  onClick={toggleMenuHandler}
+                />
+              ) : (
+                <GiHamburgerMenu
+                  className="h-6 w-6 cursor-pointer hover:fill-indigo-500 md:hidden"
+                  style={{ color: "#FFEAEC" }}
+                  onClick={toggleMenuHandler}
+                />
+              )}
+              <ul
+                className={toggleClass}
+                aria-labelledby="dropdownMenuButton1s"
+              >
+                <li>
+                  <Link
+                    to="/"
+                    className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/aboutus"
+                    className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  >
+                    About Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/products"
+                    className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  >
+                    Products
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/contactus"
+                    className="dropdown-item text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-gray-700 hover:bg-gray-100"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+              </ul>
             </div>
-        </nav>
-    )
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
-
 
 export default NavBar;

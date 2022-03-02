54bd87d819af6522b64d1731bbe8091ff2eabbca
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-03-02 18:06:21 +0600

    admin login and dashboard route added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,7 @@ import Home from ".//Home/Home";
 import ContactUs from "./ContactUs/ContactUS";
 import NotFound404 from "./404/NotFound404";
 import Login from "./admin/LogIn";
+import AdminDashBoard from "./admin/AdminDashboard";
 import "tw-elements";
 
 const Main = () => {
@@ -15,6 +16,7 @@ const Main = () => {
       <Route path="products" element={<Products />} />
       <Route path="contact-us" element={<ContactUs />} />
       <Route path="login" element={<Login />} />
+      <Route path="admin" element={<AdminDashBoard />} />
       <Route path="*" element={<NotFound404 />} />
     </Routes>
   );

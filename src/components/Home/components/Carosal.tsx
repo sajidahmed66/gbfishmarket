5dac3285f716745d84b951b7e69b690b10273341
Raisul Islam <raisulislam.50@gmail.com>
2022-03-06 01:51:46 +0600

    update

--- src/components/Home/components/Carosal.tsx
+++ src/components/Home/components/Carosal.tsx
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import sliderData from "../../../data/sliderData";
 
+
 const Carosal = () => {
-  console.log(sliderData);
   const settings = {
     dots: true,
     infinite: true,
@@ -17,25 +16,9 @@ const Carosal = () => {
   };
   return (
     <>
-    {console.log(sliderData)}
-      <div className="container max-w-fit max-h-fit">
-        <Slider {...settings} className="-z-10">
-          <div className="h-[22rem] md:h-[24rem] lg:h-[28rem] ">
-            <img
-              className="object-cover w-full h-[22rem] md:h-full"
-              src={require("../../../assets/img/banner/car-1.jpg")}
-              alt="imag"
-            />
-          </div>
-
-          <div className="h-[22rem] md:h-[24rem] lg:h-[28rem] ">
-            <img
-              className="object-cover w-full h-[22rem] md:h-full"
-              src={require("../../../assets/img/banner/car-2.jpg")}
-              alt="imag"
-            />
-          </div>
-
+      {/* {console.log(sliderData)} */}
+      <div className="">
+        <Slider {...settings}>
           <div className="h-[22rem] md:h-[24rem] lg:h-[28rem] ">
             <img
               className="object-cover w-full h-[22rem] md:h-full"
@@ -44,7 +27,23 @@ const Carosal = () => {
             />
           </div>
 
-          <div className="h-[22rem] md:h-[24rem] lg:h-[28rem] ">
+          {/* <div className="h-[22rem] md:h-[24rem] lg:h-[28rem] ">
+            <img
+              className="object-cover w-full h-[22rem] md:h-full"
+              src={require("../../../assets/img/banner/car-2.jpg")}
+              alt="imag"
+            />
+          </div> */}
+
+          {/* <div className="h-[22rem] md:h-[24rem] lg:h-[28rem] ">
+            <img
+              className="object-cover w-full h-[22rem] md:h-full"
+              src={require("../../../assets/img/banner/car-3.jpg")}
+              alt="imag"
+            />
+          </div> */}
+
+          <div className="h-[22rem] md:h-[24rem] lg:h-[28rem]">
             <img
               className="object-cover w-full h-[22rem] md:h-full"
               src={require("../../../assets/img/banner/car-4.jpg")}

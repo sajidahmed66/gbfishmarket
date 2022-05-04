e9d4a91195e27e5889bf8241f185592c9c81b479
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-05-04 09:34:04 +0600

    Point before migrating to file based routing

--- src/components/admin/AdminDashboard.tsx
+++ src/components/admin/AdminDashboard.tsx
@@ -1,5 +1,13 @@
 const AdminDashBoard = () => {
-  return <></>;
+  return (
+    // admindashboard with sidebar and content
+    <div className="grid grid-cols-4 gap-1 py-4 h-[100vh] w-[100vw]">
+      <div className="border-2 border-black "></div>
+      <div className="border-2 border-black ">2</div>
+      <div className="border-2 border-black ">3</div>
+      <div className="border-2 border-black ">4</div>
+    </div>
+  );
 };
 
 export default AdminDashBoard;

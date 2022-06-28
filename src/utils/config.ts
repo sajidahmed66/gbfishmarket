4bf778c7ed9604c5904e10f004da63a2f7d1f25a
Raisul Islam <raisulislam.50@gmail.com>
2022-06-28 18:34:23 +0600

    update

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,4 @@
 export const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API as string;
 export const GOOGLE_MAP_ID = process.env.REACT_APP_GOOGLE_MAPS_MAP_ID as string;
-console.log(process.env.REACT_APP_BASE_URL)
 export const API_URL = `${process.env.REACT_APP_BASE_URL}/api` as string;
 export const BASE_URL = process.env.REACT_APP_BASE_URL as string;

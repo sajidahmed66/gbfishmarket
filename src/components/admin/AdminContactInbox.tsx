569cd5633587a4d7c443461ecab5e54d2de844c4
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-08-01 02:13:23 +0600

    Admin contact info

--- src/components/admin/AdminContactInbox.tsx
+++ src/components/admin/AdminContactInbox.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from "react";
 import { allContactData } from "../../api/apiContactUs";
 import { userInfo } from "../../utils/auth";
-import { Container, Box, Typography, Paper, Divider } from "@mui/material";
+import { Container, Box, Typography, Divider } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 
 type IContactData = {
   id: number;
@@ -12,9 +20,40 @@ type IContactData = {
   created_at: string;
   updated_at: string;
 };
+
+interface Cloumn {
+  id: "name" | "email" | "message" | "phone" | "time";
+  label: string;
+  minWidth?: number | string;
+  align?: "right";
+  format?: (value: number) => string;
+}
+
+const columns: readonly Cloumn[] = [
+  { id: "name", label: "Name", minWidth: "15%" },
+  { id: "email", label: "Email", minWidth: "15%" },
+  { id: "message", label: "Message", minWidth: "40%" },
+  { id: "phone", label: "Phone", minWidth: "15%" },
+  { id: "time", label: "Time", minWidth: "15%" },
+];
 const AdminContactInbox = () => {
   const [contactData, setContactData] = useState<IContactData[]>([]);
   const token: string = userInfo().token;
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+
+  const handleChangePage = (event: unknown, newPage: number) => {
+    setPage(newPage);
+  };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+  // const createData =(name: string, email: string, phone:string, created_at: string )=>{
+
+  // }
   useEffect(() => {
     allContactData(token)
       .then((res) => {
@@ -24,8 +63,12 @@ const AdminContactInbox = () => {
       .catch((err) => {
         console.log(err);
       });
+    return () => {
+      setContactData([]);
+    };
   }, []);
 
+  // TODO Implement MUI Table.
   return (
     <Container maxWidth="lg">
       <Box my={4} className="p-4 ">
@@ -33,35 +76,89 @@ const AdminContactInbox = () => {
           Contacts
         </Typography>
         <Divider />
-        <Box className="p-4">
-          {contactData.length > 0 ? (
-            contactData.map((contact) => (
-              <Paper
-                key={contact.id}
-                className="flex flex-row items-center justify-around p-4 my-2 "
-              >
-                <Typography variant="h6" component="h2">
-                  {contact.name}
-                </Typography>
-                <Typography variant="body1" component="p">
-                  {contact.message}
-                </Typography>
-                <Typography variant="body1" component="p">
-                  {contact.email}
-                </Typography>
-                <Typography variant="body1" component="p">
-                  {contact.phone}
-                </Typography>
-              </Paper>
-            ))
-          ) : (
-            <Paper>
-              <Typography variant="body1" component="p">
-                No Contacts
-              </Typography>
-            </Paper>
-          )}
-        </Box>
+        {contactData.length > 0 ? (
+          <Box className="p-4">
+            <TableContainer
+              sx={{
+                maxHeight: 600,
+                width: "100%",
+                // height: 600,
+              }}
+            >
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    {columns.map((column) => (
+                      <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                      >
+                        {column.label}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {contactData.map((item) => {
+                    return (
+                      <TableRow
+                        hover
+                        role="checkbox"
+                        tabIndex={-1}
+                        key={item.id}
+                        onClick={() => console.log("clicked")}
+                      >
+                        <TableCell
+                          component="th"
+                          align="left"
+                          scope="row"
+                          sx={{ width: "15%" }}
+                        >
+                          {item.name}
+                        </TableCell>
+                        <TableCell align="left" sx={{ width: "15%" }}>
+                          {item.email}
+                        </TableCell>
+                        <TableCell align="left" sx={{ width: "40%" }}>
+                          {item.message}
+                        </TableCell>
+                        <TableCell align="left" sx={{ width: "15%" }}>
+                          {item.phone}
+                        </TableCell>
+                        <TableCell align="left" sx={{ width: "15%" }}>
+                          {item.created_at}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+              <TablePagination
+                rowsPerPageOptions={[10, 25, 100]}
+                component="div"
+                count={contactData.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </TableContainer>
+
+            {/* ) : 
+          )} */}
+          </Box>
+        ) : (
+          <Paper>
+            <Typography
+              variant="body1"
+              component="p"
+              className="flex flex-row items-center justify-center"
+            >
+              No messages yet.
+            </Typography>
+          </Paper>
+        )}
       </Box>
     </Container>
   );

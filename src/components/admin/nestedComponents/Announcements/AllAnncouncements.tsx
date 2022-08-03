a8de42932a93f5625149b071c06f96b542f3c00e
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-08-03 20:55:51 +0600

    show delete message upon anncement detetation

--- src/components/admin/nestedComponents/Announcements/AllAnncouncements.tsx
+++ src/components/admin/nestedComponents/Announcements/AllAnncouncements.tsx
@@ -3,6 +3,8 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 import { IAnnouncement } from "../AdminAnnouncement";
 import { useNavigate } from "react-router-dom";
 import AnnouncementCard from "../../components/AnnouncementCard";
@@ -18,17 +20,26 @@ const AllAnncouncements = () => {
   const [announcements, setAnnouncements] = useState<IAnnouncement[]>([]);
   const [loadingData, setLoadingData] = useState<boolean>(true);
   const [reload, setReload] = useState({});
+  const [success, setSuccess] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   const handeleDeleteAnnouncement = (id: number) => {
     const token: string = userInfo().token;
     deleteAnnouncementsById(id, token)
       .then((res) => res.data)
       .then((data) => {
-        console.log("delete announcement", data);
+        setSuccess("Announcement deleted successfully");
         setReload({});
+        setTimeout(() => {
+          setSuccess("");
+        }, 3000);
       })
       .catch((err) => {
         console.log("handledelete", err); // genarate error to see what happens
+        setError("failed to delete");
+        setTimeout(() => {
+          setError("");
+        }, 3000);
       });
   };
 
@@ -40,6 +51,7 @@ const AllAnncouncements = () => {
         setLoadingData(false);
       })
       .catch((err) => {
+        setLoadingData(false);
         console.log(err);
       });
     return () => {
@@ -51,6 +63,16 @@ const AllAnncouncements = () => {
   return (
     <Container>
       <div className="bg-slate-50">
+        <Snackbar open={!!success} autoHideDuration={6000}>
+          <Alert variant="filled" severity="success" sx={{ width: "100%" }}>
+            {success}
+          </Alert>
+        </Snackbar>
+        <Snackbar open={!!error} autoHideDuration={6000}>
+          <Alert variant="filled" severity="error" sx={{ width: "100%" }}>
+            {error}
+          </Alert>
+        </Snackbar>
         <div className="flex flex-col items-start justify-between w-full p-4 my-4 h-36 ">
           <Typography variant="h5">Create a new announcement Card </Typography>
           <button
@@ -63,6 +85,7 @@ const AllAnncouncements = () => {
       </div>
       <Divider />
       <div className="w-full mt-4">
+        {announcements.length === 0 && <div>No announcements</div>}
         {!loadingData ? (
           <Grid container spacing={2}>
             {announcements.map((item) => (

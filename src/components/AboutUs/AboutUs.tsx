0efaea472b4144373e6bbd2e7a574cb21b08fa90
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-08-11 10:35:35 +0600

    about-us page rework

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,122 +1,52 @@
 import Layout from "../Common/Layout";
-import Teams from "./components/Teams";
+import Container from "@mui/material/Container";
+import { Outlet, NavLink, useLocation } from "react-router-dom";
+
 const AboutUs = () => {
+  const location = useLocation();
   return (
     <Layout title="About Us">
-      <div className="flex flex-col items-center justify-center">
-        {/* about us slider */}
-        <div>
-          <img
-            src={require("../../assets/img/fish-2.jpg")}
-            className="w-screen h-72"
-            alt="aboutimg"
-          />
+      <Container maxWidth="lg" className="">
+        {/* about us heading */}
+        <div className="flex flex-col items-center justify-center w-full mx-auto mt-20 ">
+          <p className="text-xl md:text-2xl font-kawshan text-[#b8cc08]">
+            about us
+          </p>
+          <p className="p-4 text-2xl font-bold text-gray-800 md:text-4xl font-skModernistBold">
+            Golden Bough Aquaculture Ltd.
+          </p>
         </div>
-        {/* end of about us slider */}
+        <div className="flex flex-col w-full p-4 md:flex-row ">
+          <div className="flex flex-row flex-wrap items-center justify-start flex-1 flex-grow w-full md:flex-col md:w-1/4 ">
+            {[
+              { title: "About Us", path: "/about-us" },
+              { title: "Message from CEO", path: "/about-us/message-from-ceo" },
+              { title: "History", path: "/about-us/history" },
+              { title: "Teams", path: "/about-us/teams" },
 
-        {/* about us */}
-        <div className="flex flex-wrap max-w-screen-xl p-6 mx-auto md:flex-row md:px-8">
-          {/* about intro */}
-          <div className="w-full pr-4 md:w-1/2 ">
-            <div className="text-left">
-              <h4 className="font-bold font-skModernist text-2xl leading-8 py-4 px-1 text-[#b8cc08]">
-                About us
-              </h4>
-
-              <h1 className="pb-5 text-4xl font-bold text-gray-800 font-skModernistBold">
-                Golden Bough Aquaculture Ltd.
-              </h1>
-
-              <p className="mb-4 text-lg leading-8 text-left text-gray-600 font-skModernist">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                sed odio sit amet nibh vulputate cursus. Nullam accumsan, nulla
-                sed dictum eleifend, nunc risus consectetur purus, at elementum
-                risus nulla et nunc. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Animi placeat quibusdam numquam error fugit!
-                Eum ea quibusdam delectus earum, itaque unde, sunt dolor totam,
-                deleniti cum est laboriosam repellat similique!
-              </p>
-              <p className="mb-4 text-lg leading-8 text-left text-gray-600 font-skModernist">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                sed odio sit amet nibh vulputate cursus. Nullam accumsan, nulla
-                sed dictum eleifend, nunc risus consectetur purus, at elementum
-                risus nulla et nunc. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Animi placeat quibusdam numquam error fugit!
-                Eum ea quibusdam delectus earum, itaque unde, sunt dolor totam,
-                deleniti cum est laboriosam repellat similique!
-              </p>
-            </div>
+              {
+                title: "Company Philosophy",
+                path: "/about-us/company-philosophy",
+              },
+            ].map((item, index) => (
+              <NavLink
+                key={index}
+                className={
+                  location.pathname === item.path
+                    ? "w-full px-4 py-2 m-1 text-base font-bold text-center text-white hover:cursor-pointer sm:text-lg md:text-lg font-skModernistBold bg-slate-700"
+                    : "w-full px-4 py-2 m-1 text-base font-bold text-center bg-slate-100 hover:text-white hover:cursor-pointer sm:text-lg md:text-lg font-skModernistBold hover:bg-slate-700"
+                }
+                to={item.path}
+              >
+                {item.title}
+              </NavLink>
+            ))}
           </div>
-          {/* about image */}
-          <div className="w-5/6 h-full py-6 pl-2 md:w-1/2">
-            <div className="flex justify-end">
-              <img
-                src={require("../../assets/img/Carp-fishing.jpg")}
-                className="w-5/6 rounded-2xl"
-                alt="aboutimg"
-              />
-            </div>
-          </div>
-          {/* end of about image */}
-        </div>
-
-        {/* history */}
-        {/* heading */}
-        <div className="container flex flex-col items-center justify-center w-full max-w-screen-xl pt-4 pb-2 mx-auto">
-          <h2 className="text-5xl font-bold text-[#b8cc08] font-skModernistBold ">
-            History
-          </h2>
-          <div className="flex items-center justify-center pt-4 ">
-            <img src={require("../../assets/img/divider.png")} alt="divider" />
+          <div className="w-full md:w-3/4">
+            <Outlet />
           </div>
         </div>
-        {/* end of heading */}
-
-        {/* history content */}
-        <div className="flex flex-col items-center w-full max-w-screen-xl p-6 justify-items-center md:flex-row md:items-start md:justify-center md:mx-auto">
-          {/* history image */}
-          <div className="w-5/6 h-full py-6 mx-auto md:w-1/2 ">
-            <div className="flex justify-start">
-              <img
-                src={require("../../assets/img/Carp-fishing.jpg")}
-                className="w-5/6 rounded-2xl"
-                alt="aboutimg"
-              />
-            </div>
-          </div>
-          {/* end of history image */}
-          {/* history bok bok */}
-          <div className="w-full pl-2 pr-4 md:w-1/2">
-            <div className="text-left">
-              <h1 className="pb-5 text-4xl font-bold text-gray-800 font-skModernistBold">
-                Golden Bough Aquaculture Ltd.
-              </h1>
-
-              <p className="mb-4 text-lg leading-8 text-left text-gray-600 font-skModernist">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                sed odio sit amet nibh vulputate cursus. Nullam accumsan, nulla
-                sed dictum eleifend, nunc risus consectetur purus, at elementum
-                risus nulla et nunc. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Animi placeat quibusdam numquam error fugit!
-                Eum ea quibusdam delectus earum, itaque unde, sunt dolor totam,
-                deleniti cum est laboriosam repellat similique!
-              </p>
-              <p className="mb-4 text-lg leading-8 text-left text-gray-600 font-skModernist">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
-                sed odio sit amet nibh vulputate cursus. Nullam accumsan, nulla
-                sed dictum eleifend, nunc risus consectetur purus, at elementum
-                risus nulla et nunc. Lorem ipsum dolor sit amet consectetur
-                adipisicing elit. Animi placeat quibusdam numquam error fugit!
-                Eum ea quibusdam delectus earum, itaque unde, sunt dolor totam,
-                deleniti cum est laboriosam repellat similique!
-              </p>
-            </div>
-          </div>
-          {/* end of history bok bok */}
-        </div>
-        {/* team */}
-        <Teams />
-      </div>
+      </Container>
     </Layout>
   );
 };
---
+++ src/components/AboutUs/components/CompanyPhilosophy.tsx
@@ -0,0 +1,25 @@
+const CompanyPhilosophy = () => {
+  return (
+    <div className="max-w-screen-xl p-2 m-4 md:px-8">
+      <h5 className="pb-4 text-xl font-bold font-skModernistBold ">
+        {" "}
+        Company Philosophy
+      </h5>
+      <div className="w-full pr-4 ">
+        <div className="text-left">
+          <p className="mb-4 text-lg leading-8 text-left text-gray-600 font-skModernist">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed
+            odio sit amet nibh vulputate cursus. Nullam accumsan, nulla sed
+            dictum eleifend, nunc risus consectetur purus, at elementum risus
+            nulla et nunc. Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Animi placeat quibusdam numquam error fugit! Eum ea quibusdam
+            delectus earum, itaque unde, sunt dolor totam, deleniti cum est
+            laboriosam repellat similique!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyPhilosophy;

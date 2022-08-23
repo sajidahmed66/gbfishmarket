a0c508080df0ccceede79be9744ed146d76ac1e4
Raisul Islam <raisulislam.50@gmail.com>
2022-08-23 19:10:47 +0600

    add admin products category api

---
+++ src/api/apiAdminCategoryProducts.ts
@@ -0,0 +1,30 @@
+import { API_URL } from "../utils/config";
+import axios from "axios";
+
+export const getAllProductsCategory = () => {
+  return axios.get(`${API_URL}/api/admin/category-products`);
+};
+
+export const addProductsCategory = (data: FormData) => {
+  return axios.post(`${API_URL}/admin/home/category-products`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export const getProductsCategoryById = (id: number) => {
+  return axios.get(`${API_URL}/admin/home/category-products/${id}`);
+};
+
+export const updateProductsCategory = (data: FormData, id: number) => {
+  return axios.put(
+    `${API_URL}/admin/home/category-products/${id}`,
+    data,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  );
+};

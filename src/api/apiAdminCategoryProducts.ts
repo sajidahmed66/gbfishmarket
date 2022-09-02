e065f60f5db0d73c543249077bd4472eb57fdb88
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-09-02 21:16:07 +0600

    pull marge commit

--- src/api/apiAdminCategoryProducts.ts
+++ src/api/apiAdminCategoryProducts.ts
@@ -1,30 +1,32 @@
 import { API_URL } from "../utils/config";
 import axios from "axios";
 
-export const getAllProductsCategory = () => {
-  return axios.get(`${API_URL}/api/admin/category-products`);
-};
+// codes are defined in admin products as  category is part of the product
 
-export const addProductsCategory = (data: FormData) => {
-  return axios.post(`${API_URL}/admin/home/category-products`, data, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-};
+// export const getAllProductsCategory = () => {
+//   return axios.get(`${API_URL}/api/admin/category-products`);
+// };
 
-export const getProductsCategoryById = (id: number) => {
-  return axios.get(`${API_URL}/admin/home/category-products/${id}`);
-};
+// export const addProductsCategory = (data: FormData) => {
+//   return axios.post(`${API_URL}/admin/home/category-products`, data, {
+//     headers: {
+//       "Content-Type": "multipart/form-data",
+//     },
+//   });
+// };
 
-export const updateProductsCategory = (data: FormData, id: number) => {
-  return axios.put(
-    `${API_URL}/admin/home/category-products/${id}`,
-    data,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
-  );
-};
+// export const getProductsCategoryById = (id: number) => {
+//   return axios.get(`${API_URL}/admin/home/category-products/${id}`);
+// };
+
+// export const updateProductsCategory = (data: FormData, id: number) => {
+//   return axios.put(
+//     `${API_URL}/admin/home/category-products/${id}`,
+//     data,
+//     {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//     }
+//   );
+// };

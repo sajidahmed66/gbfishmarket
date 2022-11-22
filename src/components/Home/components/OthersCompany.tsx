47a1b8f3f277a6affaf8b8b0e5543c0e5b864523
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2022-11-22 23:20:03 +0600

    others company's logo section added

--- src/components/Home/components/OthersCompany.tsx
+++ src/components/Home/components/OthersCompany.tsx
@@ -1,33 +1,28 @@
-import React from "react";
 const OthersCompany = () => {
   return (
-    <div className="bg-white-500 ">
-      <div className="container flex flex-col md:grid md:grid-cols-3 text-center items-center justify-center w-full max-w-screen-xl px-8 xl:px-24 lg:px-24 md:px-12 sm:px-8 pt-4 pb-6 mx-auto">
-        <div className="w-36 bg-white h-36 md:h-36 ">
-          <img
-            src={require("../../../assets/img/company_1.png")}
-            className="relative w-full min-w-full h-36"
-            style={{ objectFit: "cover" }}
-            alt="aboutimg"
-          />
+    <div className="w-screen  bg-[#3a6ea5]">
+      <section className="px-4 pt-12 pb-16 mx-auto max-w-7xl">
+        <div className="grid grid-cols-2 gap-10 mx-auto text-center sm:grid-cols-3 ">
+          <div className="flex items-center justify-center">
+            <img
+              src={require("../../../assets/img/company_1.png")}
+              className="object-contain h-28 "
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src={require("../../../assets/img/logo_white.png")}
+              className="object-contain h-28 "
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <img
+              src={require("../../../assets/img/logo.png")}
+              className="object-contain h-28"
+            />
+          </div>
         </div>
-        <div className="w-36 bg-white h-36 md:h-36 ">
-          <img
-            src={require("../../../assets/img/banner_ann.jpg")}
-            className="relative w-full min-w-full h-36"
-            style={{ objectFit: "cover" }}
-            alt="aboutimg"
-          />
-        </div>
-        <div className="w-36 bg-white h-36 md:h-36 ">
-          <img
-            src={require("../../../assets/img/banner_ann.jpg")}
-            className="relative w-full min-w-full h-36"
-            style={{ objectFit: "cover" }}
-            alt="aboutimg"
-          />
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
